1f3ccd408a42612530ed45695d262c9d238b601e
karunantonyqa <karun.antony@academytrainee.com>
2018-09-04 12:18:05 +0100

    initial commit 2

---
+++ Javascript/codingChallenge2.js
@@ -0,0 +1,17 @@
+function square(a){
+    a = a*a;
+    return a;
+}
+
+function sum(a,b,c){
+    c = a + b + c;
+    return c;
+}
+
+function Person(name, age, occupation){
+    var p1 = {};
+    p1.name = name;
+    p1.age = age;
+    p1.occupation = occupation;
+    return p1;
+}
